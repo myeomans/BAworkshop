d16819ae7b253231d033d08a569e159e0b729004
Michael Yeomans <myeomans@users.noreply.github.com>
2023-04-24 12:49:23 +0100

    first

---
+++ TableOfContents.R
@@ -0,0 +1,32 @@
+################################################
+#
+#       Business Analytics NLP Workshop
+#
+#  Predicting, evaluating, and interpreting
+#
+#
+################################################
+
+# these packages are required, install if you don't have them
+library(quanteda)
+library(textclean)
+library(tidyverse)
+library(ggrepel)
+library(glmnet)
+
+# a function we use to build a model later
+source("BA_dfm.R") 
+
+
+# learn about handling text data in R
+source("text_basics.R") 
+
+# load data - restaurant reviews
+review_dat<-readRDS("review_dat.RDS")
+
+# First thing - check variables
+names(review_dat)
+dim(review_dat)
+
+# build an NLP model
+source("BA_ngrams.R")
